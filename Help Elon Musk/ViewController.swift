10bd9831718fea2d858be5218918c2f13b869bc7
rezakoushki95 <rezaaarhus@hotmail.com>
2022-09-11 05:02:43 +0200

    add button which launches and returns rocket image

--- Help Elon Musk/ViewController.swift
+++ Help Elon Musk/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var rocketImage: UIImageView!
+	@IBOutlet weak var launchOrLandButton: UIButton!
 	
 	var rocketIndex = 0
 	let numberOfRockets = 5
@@ -17,16 +18,31 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		rocketImage.image = UIImage(named: "rocket0")
+		launchOrLandButton.setTitle("Blast Off!", for: .normal)
+		print(rocketImage.frame.origin.y)
+
 	}
 
+	@IBAction func launchOrLandPressed(_ sender: UIButton) {
+		if launchOrLandButton.currentTitle == "Blast Off!" {
+			UIView.animate(withDuration: 3.0, delay: 0.0, options: []) {
+				self.rocketImage.transform = CGAffineTransform(translationX: 0, y: -(self.rocketImage.frame.origin.y + self.rocketImage.frame.size.height))
+			}
+			launchOrLandButton.setTitle("Return", for: .normal)
+		} else {
+			UIView.animate(withDuration: 3.0, delay: 0.0, options: []) {
+				self.rocketImage.transform = CGAffineTransform.identity
+			}
+			launchOrLandButton.setTitle("Blast Off!", for: .normal)
+		}
+	}
 	@IBAction func rocketTapped(_ sender: UITapGestureRecognizer) {
 		rocketIndex += 1
-
-		rocketImage.image = UIImage(named: "rocket\(rocketIndex)")
-		
-		if rocketIndex == numberOfRockets - 1 {
+		if rocketIndex == numberOfRockets {
 			rocketIndex = 0
 		}
+		rocketImage.image = UIImage(named: "rocket\(rocketIndex)")
+
 	}
 	
 }
